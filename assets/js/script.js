e50527979ecd4e3c3f0c73c227a2abc8615f1738
Seth Nightingale <sethnightingale10@gmail.com>
2021-07-05 13:20:21 +0800

    Setting up buttons and trying to start on localstorage saving
    Some of the more important functions, this is hopefully just  going to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
-var NowMoment = moment();
-var eDisplayMoment = document.getElementById('currentDay');
-eDisplayMoment.innerHTML = NowMoment.format('DD-MM-YYYY');
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+
+document.getElementById('currentDay').innerHTML = ("The date is " + today);
+
+var saveButton = document.querySelector(".save-button");
+var workOne = document.querySelector(".work-one");
+
+var textOne = window.localStorage.getItem("textOne");
+
+saveButton.addEventListener("click", function() {
+    workOne.textContent = textOne;
+    window.localStorage.setItem("textOne", textOne);
+})
+
